1b7614004dc84884ffd9c93c506a779e4e729077
RAGHAVENDRA SWAMY <32408841+Raghavendraswamy@users.noreply.github.com>
2024-09-20 22:22:03 +0530

    Create unittestcases.py

---
+++ unittestcases.py
@@ -0,0 +1,46 @@
+import unittest
+from multiply import multipilcation
+class MultiplyTestCase(unittest.TestCase):
+
+   def test_1(self):
+
+       result = multipilcation(3, 4)
+
+       self.assertEqual(result, 12)
+
+   def test_2(self):
+
+       result = multipilcation(3, -4)
+
+       self.assertEqual(result, -12)
+
+   def test_3(self):
+
+       result = multipilcation(-3, -4)
+
+       self.assertEqual(result, 12)
+       
+   def test_4(self):
+
+       result = multipilcation(3, 0)
+
+       self.assertEqual(result, 0)
+
+
+   def test_5(self):
+
+       result = multipilcation(-3, 0)
+
+       self.assertEqual(result,0)
+       
+   def test_6(self):
+
+        result = multipilcation(3, 1)
+
+        self.assertEqual(result,3)
+      
+    
+
+       
+if __name__ == '__main__':
+    unittest.main()
